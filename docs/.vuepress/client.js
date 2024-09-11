99096f769609ef617f00b6cd1fe75446d36c65d7
Luis Cabrera Benito <parzibyte@gmail.com>
2024-09-10 19:14:47 -0600

    Tailwind

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,9 +1,11 @@
 import { defineClientConfig } from 'vuepress/client'
 import MiBoton from './components/MiBoton.vue'
-
+import Playground from './components/Playground.vue'
+import './styles/estilos.css'
 export default defineClientConfig({
   enhance({ app }) {
     // Registrar el componente globalmente
     app.component('MiBoton', MiBoton)
+    app.component('Playground', Playground)
   },
 })
